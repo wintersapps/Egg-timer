a69b50bf3af17f2e6e640be0eff4c1cc576c4bf8
Patryk Winter <patrykwintersapps@gmail.com>
2020-11-01 12:22:13 +0100

    Initial Commit

---
+++ Egg timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Egg timer
+//
+//  Created by Patryk Winter on 01/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
